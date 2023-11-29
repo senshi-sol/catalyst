139d34b1b4ad807aca644f7b70ce36891b40967a
vivekpal1 <31vivekpal@gmail.com>
2023-11-29 22:28:43 +0530

    fix: build

--- packages/ui/components/checkbox/checkbox-group.tsx
+++ packages/ui/components/checkbox/checkbox-group.tsx
@@ -1,7 +1,6 @@
-import {forwardRef} from "@nextui-org/system";
-
-import {CheckboxGroupProvider} from "./checkbox-group-context";
-import {UseCheckboxGroupProps, useCheckboxGroup} from "./use-checkbox-group";
+import { forwardRef } from "@nextui-org/system";
+import { CheckboxGroupProvider } from "./checkbox-group-context";
+import { UseCheckboxGroupProps, useCheckboxGroup } from "./use-checkbox-group";
 
 export interface CheckboxGroupProps extends UseCheckboxGroupProps {}
 
@@ -17,16 +16,40 @@ const CheckboxGroup = forwardRef<"div", CheckboxGroupProps>((props, ref) => {
     getWrapperProps,
     getDescriptionProps,
     getErrorMessageProps,
-  } = useCheckboxGroup({...props, ref});
+  } = useCheckboxGroup({ ...props, ref });
+
+  // Updated dummy ValidationResult with a mock ValidityState
+  const dummyValidationResult = {
+    isInvalid: false, // or true, depending on your scenario
+    validationErrors: [], // or an array of error messages
+    validationDetails: {
+      badInput: false,
+      customError: false,
+      patternMismatch: false,
+      rangeOverflow: false,
+      rangeUnderflow: false,
+      stepMismatch: false,
+      tooLong: false,
+      tooShort: false,
+      typeMismatch: false,
+      valid: true,
+      valueMissing: false,
+      // ... add other properties of ValidityState as needed
+    }
+  };
 
   return (
     <div {...getGroupProps()}>
       {label && <span {...getLabelProps()}>{label}</span>}
       <div {...getWrapperProps()}>
-        <CheckboxGroupProvider value={context}>{children}</CheckboxGroupProvider>
+        <CheckboxGroupProvider value={context}>
+          {children}
+        </CheckboxGroupProvider>
       </div>
       {errorMessage ? (
-        <div {...getErrorMessageProps()}>{errorMessage}</div>
+        <div {...getErrorMessageProps()}>
+          {typeof errorMessage === 'function' ? errorMessage(dummyValidationResult) : errorMessage}
+        </div>
       ) : description ? (
         <div {...getDescriptionProps()}>{description}</div>
       ) : null}
